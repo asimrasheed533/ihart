74cd5c52086a3c05c690501ffbe447cb3d9ca3b3
Asim Rasheed <asimrasheed442@gmail.com>
2023-05-12 13:53:27 +0500

    fix

--- src/compounts/ContectSection.jsx
+++ src/compounts/ContectSection.jsx
@@ -3,92 +3,94 @@ import { Link } from "react-router-dom";
 export default function ContectSection() {
   return (
     <>
-      <div className="contect__contaier">
-        <div className="contect__contaier__col">
-          <div className="contect__contaier__col__heading">
-            Request for a call back
-          </div>
-          <div className="contect__contaier__col__form">
-            <form action="#">
-              <div className="input__wraper">
-                <div className="input__wraper__entry">
-                  <input
+      <div className="contect__wraper__main">
+        <div className="contect__contaier">
+          <div className="contect__contaier__col">
+            <div className="contect__contaier__col__heading">
+              Request for a call back
+            </div>
+            <div className="contect__contaier__col__form">
+              <form action="#">
+                <div className="input__wraper">
+                  <div className="input__wraper__entry">
+                    <input
+                      className="input__wraper__entry__input"
+                      type="text"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div className="input__wraper__entry">
+                    <input
+                      className="input__wraper__entry__input"
+                      type="number"
+                      placeholder="Phone"
+                    />
+                  </div>
+                </div>
+                <div className="input__wraper">
+                  <div className="input__wraper__entry">
+                    <input
+                      className="input__wraper__entry__input"
+                      type="email"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="input__wraper__entry">
+                    <input
+                      className="input__wraper__entry__input"
+                      type="text"
+                      placeholder="Organization Name"
+                    />
+                  </div>
+                </div>
+                <div className="input__wraper">
+                  <textarea
                     className="input__wraper__entry__input"
-                    type="text"
-                    placeholder="Name"
+                    placeholder="Message here..."
+                    id="#"
                   />
                 </div>
-                <div className="input__wraper__entry">
-                  <input
-                    className="input__wraper__entry__input"
-                    type="number"
-                    placeholder="Phone"
-                  />
-                </div>
+              </form>
+              <div className="btn__md__more">
+                <Link className="about__md__more__btn">
+                  Request for a Call Back
+                </Link>
               </div>
-              <div className="input__wraper">
-                <div className="input__wraper__entry">
-                  <input
-                    className="input__wraper__entry__input"
-                    type="email"
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="input__wraper__entry">
-                  <input
-                    className="input__wraper__entry__input"
-                    type="text"
-                    placeholder="Organization Name"
-                  />
-                </div>
+            </div>
+          </div>
+          <div className="contect__contaier__col">
+            <div className="contect__contaier__col__text__heading">Contact</div>
+            <div className="contect__contaier__col__text__entry">
+              <div className="contect__contaier__col__text__entry__heading">
+                Address:
               </div>
-              <div className="input__wraper">
-                <textarea
-                  className="input__wraper__entry__input"
-                  placeholder="Message here..."
-                  id="#"
-                />
+              <div className="contect__contaier__col__text__entry__sub__heading">
+                1300 Pennsylvania Avenue, NW #190-635 Washington, DC 20004 USA
               </div>
-            </form>
-            <div className="btn__md__more">
-              <Link className="about__md__more__btn">
-                Request for a Call Back
-              </Link>
             </div>
-          </div>
-        </div>
-        <div className="contect__contaier__col">
-          <div className="contect__contaier__col__text__heading">Contact</div>
-          <div className="contect__contaier__col__text__entry">
-            <div className="contect__contaier__col__text__entry__heading">
-              Address:
+            <div className="contect__contaier__col__text__entry">
+              <div className="contect__contaier__col__text__entry__heading">
+                Tel:
+              </div>
+              <div className="contect__contaier__col__text__entry__sub__heading">
+                +1 (202) 800 9099
+              </div>
             </div>
-            <div className="contect__contaier__col__text__entry__sub__heading">
-              1300 Pennsylvania Avenue, NW #190-635 Washington, DC 20004 USA
+            <div className="contect__contaier__col__text__entry">
+              <div className="contect__contaier__col__text__entry__heading">
+                Email:
+              </div>
+              <div className="contect__contaier__col__text__entry__sub__heading">
+                info@ihartglobal.org
+              </div>
             </div>
-          </div>
-          <div className="contect__contaier__col__text__entry">
-            <div className="contect__contaier__col__text__entry__heading">
-              Tel:
-            </div>
-            <div className="contect__contaier__col__text__entry__sub__heading">
-              +1 (202) 800 9099
-            </div>
-          </div>
-          <div className="contect__contaier__col__text__entry">
-            <div className="contect__contaier__col__text__entry__heading">
-              Email:
-            </div>
-            <div className="contect__contaier__col__text__entry__sub__heading">
-              info@ihartglobal.org
-            </div>
-          </div>
-          <div className="contect__contaier__col__text__entry">
-            <div className="contect__contaier__col__text__entry__heading">
-              Website:
-            </div>
-            <div className="contect__contaier__col__text__entry__sub__heading">
-              www.ihartglobal.org
+            <div className="contect__contaier__col__text__entry">
+              <div className="contect__contaier__col__text__entry__heading">
+                Website:
+              </div>
+              <div className="contect__contaier__col__text__entry__sub__heading">
+                www.ihartglobal.org
+              </div>
             </div>
           </div>
         </div>
