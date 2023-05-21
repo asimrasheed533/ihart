1c29947b0506c555611869e5e82e68b79c70dfd3
Asim Rasheed <asimrasheed442@gmail.com>
2023-05-21 11:58:45 +0500

    solution all pages done animation

--- src/screens/solution/Executive.jsx
+++ src/screens/solution/Executive.jsx
@@ -1,132 +1,149 @@
 import { Head } from "../../../modules";
-
+import Fade from "react-reveal/Fade";
 export default function Executive() {
   return (
     <>
       <Head title="Executive | I-HART" />
       <div className="solution__container">
         <div className="solution__text__wraper">
-          <div className="solution__text__wraper__heading">
-            Health Executives Academy for Leadership Support (HEALS) – a joint
-            partnership with AAH LLC
+          <Fade bottom>
+            <div className="solution__text__wraper__heading">
+              Health Executives Academy for Leadership Support (HEALS) – a joint
+              partnership with AAH LLC
+            </div>
+          </Fade>
+        </div>
+        <Fade bottom>
+          <div className="solution__contect__section__sub__heading">
+            HEALS provides an innovative and strategic training course for
+            senior health care professionals and managers to enable them to lead
+            hospitals, health care systems and medical organizations in the 21st
+            century. The curriculum focuses on the needs of senior-most
+            healthcare leaders emerging and developing markets to help achieve
+            transformation of hospitals and healthcare organizations to the
+            level of best systems in North America. The curriculum is tailored
+            for the participants and integrated modifications are made to the
+            core curriculum according to the special experiences of American
+            healthcare systems, hospitals and medical groups and non-profitable
+            organizations, for the purpose of training modern hospital
+            presidents, chief executive officers and other C-suite executives as
+            well as clinical department chairs and directors.
+            <br />
+            Each course is customized and tailored to the needs of attendees and
+            their organizations, with special consideration to their local,
+            regional and national needs and capabilities. Executives and
+            organizations have the option of choosing a course in North America
+            or their home country. Each course is 2-4 weeks in duration, based
+            on mutually identified needs and goals.
+          </div>
+        </Fade>
+      </div>
+      <Fade bottom>
+        <div className="executives__container">
+          <div className="executives__container__heading">
+            Intended audience:
+          </div>
+
+          <div className="about__contect__section__sub__heading__entry">
+            <span style={{ color: "red" }}>1: </span>
+            Presidents, Chief Executive Officers, other C-suite executives and
+            senior administrative personnel from across healthcare systems and
+            hospitals
+          </div>
+          <div className="about__contect__section__sub__heading__entry">
+            <span style={{ color: "red" }}>2: </span>
+            Department directors of large-scale hospitals with development
+            potential and self-development willingness
+          </div>
+          <div className="about__contect__section__sub__heading__entry">
+            <span style={{ color: "red" }}>3: </span>
+            Other senior key staff in medical colleges, e.g. division heads,
+            institute directors, etc.
+          </div>
+          <div className="about__contect__section__sub__heading__entry">
+            <span style={{ color: "red" }}>4: </span>
+            Investors in medical and health industry
           </div>
         </div>
-        <div className="solution__contect__section__sub__heading">
-          HEALS provides an innovative and strategic training course for senior
-          health care professionals and managers to enable them to lead
-          hospitals, health care systems and medical organizations in the 21st
-          century. The curriculum focuses on the needs of senior-most healthcare
-          leaders emerging and developing markets to help achieve transformation
-          of hospitals and healthcare organizations to the level of best systems
-          in North America. The curriculum is tailored for the participants and
-          integrated modifications are made to the core curriculum according to
-          the special experiences of American healthcare systems, hospitals and
-          medical groups and non-profitable organizations, for the purpose of
-          training modern hospital presidents, chief executive officers and
-          other C-suite executives as well as clinical department chairs and
-          directors.
-          <br />
-          Each course is customized and tailored to the needs of attendees and
-          their organizations, with special consideration to their local,
-          regional and national needs and capabilities. Executives and
-          organizations have the option of choosing a course in North America or
-          their home country. Each course is 2-4 weeks in duration, based on
-          mutually identified needs and goals.
+      </Fade>
+      <Fade bottom>
+        <div className="executives__container">
+          <div className="executives__container__heading">
+            Training locations:
+          </div>
+          <div className="about__contect__section__sub__heading__entry">
+            <span style={{ color: "red" }}>1: </span>
+            Baltimore-Washington Metropolitan areas, USA
+          </div>
+          <div className="about__contect__section__sub__heading__entry">
+            <span style={{ color: "red" }}>2: </span>
+            Other training sites in the US customized to special persons and
+            group upon mutual agreement
+          </div>
+          <div className="about__contect__section__sub__heading__entry">
+            <span style={{ color: "red" }}>3: </span>
+            Participants’ home country
+          </div>
+          For the US based courses, access to world class hospitals and site
+          visits are integrated into the schedule. For courses based in the home
+          country of participating organization, we will arrange discovery and
+          solutions development visits within their organization and other
+          organization based upon mutual agreements.
         </div>
-      </div>
-      <div className="executives__container">
-        <div className="executives__container__heading">Intended audience:</div>
-        <div className="about__contect__section__sub__heading__entry">
-          <span style={{ color: "red" }}>1: </span>
-          Presidents, Chief Executive Officers, other C-suite executives and
-          senior administrative personnel from across healthcare systems and
-          hospitals
+      </Fade>
+      <Fade bottom>
+        <div className="executives__container">
+          <div className="executives__container__heading">Teaching modes:</div>
+          Combination of class-room teaching, lectures, seminars, workshops,
+          on-site investigation and observation, meet-the-expert sessions, and
+          specialized exchanges.
         </div>
-        <div className="about__contect__section__sub__heading__entry">
-          <span style={{ color: "red" }}>2: </span>
-          Department directors of large-scale hospitals with development
-          potential and self-development willingness
+      </Fade>
+      <Fade bottom>
+        <div className="executives__container">
+          <div className="executives__container__heading">
+            Proposed Training Curriculum:
+          </div>
+          The proposed curriculum will cover key concepts, knowledge, skills and
+          experiences needed to manage and lead modern hospitals in the 21st
+          century. These will include modules on the following types of issues:
+          <div className="executive__text__wraper__entry">
+            Leadership traits
+          </div>
+          <div className="executive__text__wraper__entry">
+            Clinical service development
+          </div>
+          <div className="executive__text__wraper__entry">
+            Hospital operations forecasting
+          </div>
+          <div className="executive__text__wraper__entry">
+            Reimbursement models
+          </div>
+          <div className="executive__text__wraper__entry">
+            Hospital operation management and strategies
+          </div>
+          <div className="executive__text__wraper__entry">
+            Hospital supply chain management
+          </div>
+          <div className="executive__text__wraper__entry">
+            Hospital human resource management
+          </div>
+          <div className="executive__text__wraper__entry">
+            Hospital brand strategies
+          </div>
+          <div className="executive__text__wraper__entry">
+            Hospital and social public welfare
+          </div>
+          <div className="executive__text__wraper__entry">
+            Managing Quality of Care
+          </div>
+          <div className="executive__text__wraper__entry">Patient Safety</div>
+          <div className="executive__text__wraper__entry">
+            Physician staffing models and engagement
+          </div>
+          <div className="executive__text__wraper__entry">Case studies</div>
         </div>
-        <div className="about__contect__section__sub__heading__entry">
-          <span style={{ color: "red" }}>3: </span>
-          Other senior key staff in medical colleges, e.g. division heads,
-          institute directors, etc.
-        </div>
-        <div className="about__contect__section__sub__heading__entry">
-          <span style={{ color: "red" }}>4: </span>
-          Investors in medical and health industry
-        </div>
-      </div>
-      <div className="executives__container">
-        <div className="executives__container__heading">
-          Training locations:
-        </div>
-        <div className="about__contect__section__sub__heading__entry">
-          <span style={{ color: "red" }}>1: </span>
-          Baltimore-Washington Metropolitan areas, USA
-        </div>
-        <div className="about__contect__section__sub__heading__entry">
-          <span style={{ color: "red" }}>2: </span>
-          Other training sites in the US customized to special persons and group
-          upon mutual agreement
-        </div>
-        <div className="about__contect__section__sub__heading__entry">
-          <span style={{ color: "red" }}>3: </span>
-          Participants’ home country
-        </div>
-        For the US based courses, access to world class hospitals and site
-        visits are integrated into the schedule. For courses based in the home
-        country of participating organization, we will arrange discovery and
-        solutions development visits within their organization and other
-        organization based upon mutual agreements.
-      </div>
-      <div className="executives__container">
-        <div className="executives__container__heading">Teaching modes:</div>
-        Combination of class-room teaching, lectures, seminars, workshops,
-        on-site investigation and observation, meet-the-expert sessions, and
-        specialized exchanges.
-      </div>
-      <div className="executives__container">
-        <div className="executives__container__heading">
-          Proposed Training Curriculum:
-        </div>
-        The proposed curriculum will cover key concepts, knowledge, skills and
-        experiences needed to manage and lead modern hospitals in the 21st
-        century. These will include modules on the following types of issues:
-        <div className="executive__text__wraper__entry">Leadership traits</div>
-        <div className="executive__text__wraper__entry">
-          Clinical service development
-        </div>
-        <div className="executive__text__wraper__entry">
-          Hospital operations forecasting
-        </div>
-        <div className="executive__text__wraper__entry">
-          Reimbursement models
-        </div>
-        <div className="executive__text__wraper__entry">
-          Hospital operation management and strategies
-        </div>
-        <div className="executive__text__wraper__entry">
-          Hospital supply chain management
-        </div>
-        <div className="executive__text__wraper__entry">
-          Hospital human resource management
-        </div>
-        <div className="executive__text__wraper__entry">
-          Hospital brand strategies
-        </div>
-        <div className="executive__text__wraper__entry">
-          Hospital and social public welfare
-        </div>
-        <div className="executive__text__wraper__entry">
-          Managing Quality of Care
-        </div>
-        <div className="executive__text__wraper__entry">Patient Safety</div>
-        <div className="executive__text__wraper__entry">
-          Physician staffing models and engagement
-        </div>
-        <div className="executive__text__wraper__entry">Case studies</div>
-      </div>
+      </Fade>
     </>
   );
 }
